871164993fedb4375159c9173e4c7c31d720d4a6
moosicalex <moosicalex2@gmail.com>
2021-09-10 17:48:59 -0600

    allowed hosts change

--- week3/Superhero/config/settings.py
+++ week3/Superhero/config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-$(p38ic5dsojgjbsp%qg5sls_fk6nmcrutkqh@$u0%$ff#vykj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['moosicalex.pythonanywhere.com']
 
 
 # Application definition
