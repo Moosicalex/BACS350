1d73c2975d50ef8866934ef0a6a681c283957175
moosicalex <moosicalex2@gmail.com>
2021-09-17 18:00:01 -0600

    Custom views that I actually understood

--- week4/Superhero/config/urls.py
+++ week4/Superhero/config/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from hero.views import IndexView
+from hero.views import IndexView, HeroListView, HeroDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', IndexView.as_view()),
+    path('hero/', HeroListView.as_view()),
+    path('hero/<int:pk>', HeroDetailView.as_view()),
 ]
