1e53a0a0248b820b70f77da2e08c819c673879f3
moosicalex <moosicalex2@gmail.com>
2021-09-20 14:45:33 -0600

    I fixed something I guess

--- week4/Superhero/config/settings.py
+++ week4/Superhero/config/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "static_assets"
 
 #enable python anywhere 
 ALLOWED_HOSTS = ['moosicalex.pythonanywhere.com', '127.0.0.1']
