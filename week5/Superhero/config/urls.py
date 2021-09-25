9c9fcd63e3eecb94428ae7313f66babadd75e1cf
moosicalex <moosicalex2@gmail.com>
2021-09-24 18:08:36 -0600

    end of lesson 13

--- week5/Superhero/config/urls.py
+++ week5/Superhero/config/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from hero.views import HeroView, HeroDetail
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', IndexView.as_view()),
+    path('', HeroView.as_view()),
+    path('<int:pk>', HeroDetail.as_view()),
 ]
--- week5/Superhero/hero/views.py
+++ week5/Superhero/hero/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
-# Create your views here.
+from .models import Hero
+
+
+class HeroView(ListView):
+    template_name = 'hero_list.html'
+    model = Hero
+
+class HeroDetail(DetailView):
+    template_name = 'hero_detail.html'
+    model = Hero
