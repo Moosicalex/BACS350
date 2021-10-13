5f1ae3db2681368f25ea82eab9b76ddaf71922b2
moosicalex <moosicalex2@gmail.com>
2021-10-13 11:23:35 -0600

    I actually feel like I know how to do this

---
+++ week6/Superhero/hero/views.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, RedirectView, UpdateView
+
+from .models import Hero
+
+class HeroCreateView(CreateView):
+    model = Hero
+    template_name = 'hero_add.html'
+    fields = ['name', 'identity']
+
+class HeroView(RedirectView):
+    url = '/hero/'
+
+class HeroListView(ListView):
+    template_name = 'hero_list.html'
+    model = Hero
+
+class HeroDetailView(DetailView):
+    template_name = "hero_detail.html"
+    model = Hero
+
+class HeroUpdateView(UpdateView):
+    template_name = "hero_edit.html"
+    model = Hero
+    fields = ['name', 'identity']
+
+class HeroDeleteView(DeleteView):
+    model = Hero
+    template_name = 'hero_delete.html'
+    success_url = reverse_lazy('hero_list')
