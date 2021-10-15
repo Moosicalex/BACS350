d8df5da74ce259a9a8874baba663001a5c3d7b22
moosicalex <moosicalex2@gmail.com>
2021-10-15 14:42:30 -0600

    taking jordan to work

--- week7/Superhero/hero/models.py
+++ week7/Superhero/hero/models.py
@@ -4,10 +4,10 @@ from django.urls.base import reverse_lazy
 class Hero(models.Model):
     name = models.CharField(max_length=100)
     identity = models.CharField(max_length=100)
-    description = models.TextField()
-    strength = models.CharField(max_length=100)
-    weakness = models.CharField(max_length=100)
-    image = models.CharField(max_length=100)
+    description = models.TextField(default="Enter Description Here")
+    strength = models.CharField(max_length=100, default="Enter Strength Here")
+    weakness = models.CharField(max_length=100, default="Enter Weakness Here")
+    image = models.CharField(max_length =1000, default="\static\images\Bear.200.png")
 
     def get_absolute_url(self):
         return reverse_lazy('hero_detail', args=[str(self.id)])
